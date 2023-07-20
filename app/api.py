bff720feb4a6d9d9cfae26046761e8fc95ca8159
Mikhail Postnikov <postnikovmu@mail.ru>
2023-07-20 20:45:25 +0300

    country, programming language

--- app/api.py
+++ app/api.py
@@ -26,22 +26,57 @@ app.add_middleware(
         allow_headers=["*"]
 )
 
+df = pd.read_csv('./data/survey_results_public.csv')
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
+@app.get("/country/")
+def country():
+    countries = df.Country.unique().tolist()
+    countries_str = []
+    for country in countries:
+        if type(country) == str:
+            countries_str.append(country)
+
+    countries_str.sort()
+    return {"countries": countries_str}
+
+
+@app.get("/languagehaveworkedwith/")
+def languagehaveworkedwith():
+    print('start')
+    languages = df.LanguageHaveWorkedWith.unique().tolist()
+    languages_str = set()
+    for language in languages:
+        if type(language) != str:
+            continue
+        one_person_languages = language.split(';')
+        for one_person_language in one_person_languages:
+            if type(one_person_language) == str:
+                languages_str.add(one_person_language)
+    languages_str_list = list(languages_str)
+    languages_str_list.sort()
+    print(languages_str_list)
+    return {"languagehaveworkedwith": languages_str_list}
+
+
 @app.post("/so-da/")
 def so_da(query: Query):
     print("start")
-    df = pd.read_csv('./data/survey_results_public.csv')
 
+    res_df = df.copy()
     if query.country:
-        df = df[(df['Country'] == query.country)]
+        res_df = res_df[(res_df['Country'] == query.country)]
+    if query.languageHaveWorkedWith:
+        res_df = res_df.dropna(subset=['LanguageHaveWorkedWith'])
+        res_df = res_df[(res_df['LanguageHaveWorkedWith'].str.contains(query.languageHaveWorkedWith))]
 
     print(query)
-    result = len(df)
+    result = len(res_df)
     print(result)
     print("finish")
     return {"Number of developers": result}
